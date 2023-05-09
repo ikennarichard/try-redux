b3bb27b108940cfdf046f4e95f881d6c188f1e87
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-08 18:12:25 -1100

    Provide store to the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
