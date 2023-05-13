b0a8a359ffb29269b0b87297103f07dea844820d
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-12 23:47:29 -1100

    Update state slice

--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+  return fetch(url).then(res=> res.json()).catch(err => console.log(err))
+})
 
 const initialState = {
   cartItems: [],
@@ -11,7 +17,59 @@ const cartSlice = createSlice(
   {
     name: 'cart',
     initialState,
+
+    reducers: {
+      //immer library helps us
+      //modify the state
+      clearCart: (state) => {
+        state.cartItems = [];
+      },
+
+      removeItem: (state, action) => {
+        const id = action.payload;
+        state.cartItems = state.cartItems.filter((item) => item.id !== id)
+      },
+
+      increase: (state, { payload }) => {
+         const cartItem = state.cartItems.find((item) => item.id === payload.id);
+         cartItem.amount = cartItem.amount + 1
+      },
+
+      decrease: (state, { payload }) => {
+        const cartItem = state.cartItems.find((item) => item.id === payload.id);
+        cartItem.amount = cartItem.amount - 1
+     },
+
+     calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total.toFixed(2);
+     } 
+    },
+
+    extraReducers: {
+      [getCartItems.pending]: (state) => {
+        state.isLoading = true;
+      },
+
+      [getCartItems.fulfilled]: (state, action) => {
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      },
+
+      [getCartItems.rejected]: (state) => {
+        state.isLoading = false;
+      }
+    }
   }
 )
 
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions
+//export cartSlice obj 
+//reducer property
 export default cartSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+//immport  state slice 
 import cartReducer from './cart/cartSlice.js'
-
+import modalReducer from './modal/modalSlice.js'
 export const store = configureStore({
   reducer: {
+    //cartReducer controls state in 
+    //cartSlice
     cart: cartReducer,
+    modal: modalReducer,
   }
 })
