2f0b3297287b790b81829be9a47d121c19fa8e23
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-12 23:46:06 -1100

    Update app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react"
+import CartContainer from "./components/CartContainer"
 import Navbar from "./components/Navbar"
+import Modal from "./components/Modal"
+import { useSelector, useDispatch } from "react-redux"
+import { getCartItems } from "./redux/cart/cartSlice"
+
 function App() {
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+  const dispatch  = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch, cartItems]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   return (
-    <Navbar />
+    <main>
+      <Modal />
+      <Navbar />
+      <CartContainer />
+    </main>
   )
 }
 
